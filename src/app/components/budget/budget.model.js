87bc52da691c8818a5184c0ab9ff590d0498f200
Jin Xu <jin.xu@ef.com>
2018-05-29 16:38:27 +0800

    feat: add add budget page

--- src/app/components/budget/budget.model.js
+++ src/app/components/budget/budget.model.js
@@ -1,3 +1,6 @@
+/// <reference path="../../../../typings/index.d.ts" />
+var moment = require("moment");
+
 import { Inject } from "../../common/decorators";
 
 @Inject("api")
@@ -15,5 +18,64 @@ export default class Budget {
       return;
     }
     this.api.budget.add(budget, success);
+  }
+
+  get(budget, success, failure) {
+    var start = moment(budget.start);
+    var end = moment(budget.end);
+
+    // varify start and end
+    if (budget.start.trim().length === 0 || budget.end.trim().length === 0) {
+      failure("Budget start or end date should not be empty!");
+      return;
+    }
+    if (moment.duration(end.diff(start)).as("days")) {
+      failure("Budget end date should not later than start date!");
+      return;
+    }
+
+    var start_M = start.month() + 1;
+    var start_D = start.date();
+    var end_M = end.month() + 1;
+    var end_D = end.date();
+
+    // get all data
+    this.api.budget.all((allData) => {
+      // get all month covered
+      var monthes = [];
+      allData.forEach(function(data) {
+        var month = parseInt(data.month.split("-").pop());
+        var amount = data.amount;
+        if (month >= start_M && month <= end_M) monthes.push({ month, amount });
+      });
+
+      // case for length 0
+      if (monthes.length === 0) success(0);
+      //  case for length 1
+      else if (monthes.length === 1) {
+        var days = moment.duration(end.diff(start)).as("days") + 1;
+        var totalDays = start.daysInMonth();
+
+        success((days / totalDays) * monthes[0].amount);
+      }
+      // case for multi-monthes
+      else {
+        var result;
+        var fullMonthes = monthes.splice(1, monthes.length - 2);
+        var firstMonth = monthes[0];
+        var lastMonth = monthes[1];
+
+        firstMonth.amount = (firstMonth.amount * (start.daysInMonth() - start_D + 1)) / start.daysInMonth();
+        lastMonth.amount = (lastMonth.amount * end_D) / end.daysInMonth();
+
+        var finalMonth = monthes.concat(fullMonthes);
+
+        result = finalMonth.reduce(function(acc, _month) {
+          return acc + _month.amount;
+        }, 0);
+
+        success(result);
+      }
+    });
   }
 }
---
+++ src/app/components/budget/get.controller.js
@@ -0,0 +1,30 @@
+import { Inject } from "../../common/decorators";
+
+@Inject("budgetModel", "$state")
+export default class BudgetGetController {
+  constructor(budgets, $state) {
+    const currentTime = new Date();
+    this.budgets = budgets;
+    this.$state = $state;
+    this.budget = {
+      start: `${currentTime.getFullYear()}-${currentTime.getMonth() + 1}-${currentTime.getDate()}`,
+      end: `${currentTime.getFullYear()}-${currentTime.getMonth() + 1}-${currentTime.getDate()}`,
+      sum: void 0
+    };
+    this.message = "";
+  }
+
+  get() {
+    // reset msg
+    this.message = "";
+
+    // call model service
+    this.budgets.get(
+      this.budget,
+      (result) => {
+        this.budget.sum = result;
+      },
+      (err) => (this.message = err)
+    );
+  }
+}
