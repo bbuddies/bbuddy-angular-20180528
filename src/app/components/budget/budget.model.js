5b656415daa4bdfe5d8a5764648731d5e7d49b81
Jin Xu <jin.xu@ef.com>
2018-05-30 14:33:03 +0800

    refactor: wrap the logic with functions

--- src/app/components/budget/budget.model.js
+++ src/app/components/budget/budget.model.js
@@ -3,6 +3,68 @@ var moment = require("moment");
 
 import { Inject } from "../../common/decorators";
 
+function genEmptyMonths(start, end) {
+  const startYear = start.year();
+  const startMonth = start.month() + 1;
+  const endYear = end.year();
+  const endMonth = end.month() + 1;
+
+  let durationMonth = parseInt(moment.duration(end.diff(start)).as("month")) + 1;
+  let count = 0;
+  let monthsObj = new Array(durationMonth);
+
+  for (let _year = startYear; _year <= endYear * 2 - startYear; _year++) {
+    let _startMonth = _year === startYear ? startMonth : 0;
+    let _endMonth = _year === endYear ? endMonth : 12;
+    for (let _month = _startMonth; _month <= _endMonth; _month++) {
+      monthsObj[count] = {
+        year: _year,
+        month: _month,
+        amount: 0
+      };
+      count++;
+    }
+  }
+  return monthsObj;
+}
+
+function dataApplyToMonths(budget, monthsObj) {
+  budget.forEach((data) => {
+    const yearMonth = data.month.split("-");
+    const year = parseInt(yearMonth[0]);
+    const month = parseInt(yearMonth[1]);
+    const matchedMonth = monthsObj.filter((_data) => {
+      return _data.year === year && _data.month === month;
+    })[0];
+    matchedMonth && (matchedMonth.amount = data.amount);
+  });
+}
+
+function calcSum(start, end, monthsObj) {
+  const startDay = start.date();
+  const endDay = end.date();
+
+  let sum;
+  // case for length 0
+  if (monthsObj.length === 0) sum = 0;
+  //  case for length 1
+  else if (monthsObj.length === 1) {
+    const days = moment.duration(end.diff(start)).as("days") + 1;
+    const totalDays = start.daysInMonth();
+
+    sum = (days / totalDays) * monthsObj[0].amount;
+  }
+  // case for multi-monthes
+  else {
+    monthsObj[0].amount *= (start.daysInMonth() - startDay + 1) / start.daysInMonth();
+    monthsObj[monthsObj.length - 1].amount *= endDay / end.daysInMonth();
+
+    sum = monthsObj.reduce((acc, _month) => acc + _month.amount, 0);
+  }
+
+  return sum;
+}
+
 @Inject("api")
 export default class Budget {
   constructor(api) {
@@ -30,65 +92,16 @@ export default class Budget {
 
     const start = moment(budget.start);
     const end = moment(budget.end);
+
     if (moment.duration(end.diff(start)).as("days") < 0) {
       failure("Budget end date should not later than start date!");
       return;
     }
 
-    const startYear = start.year();
-    const startMonth = start.month() + 1;
-    const startDay = start.date();
-    const endYear = end.year();
-    const endMonth = end.month() + 1;
-    const endDay = end.date();
-
-    // get all data
-    this.api.budget.all((allData) => {
-      const durationYear = endYear - startYear;
-      let durationMonth = parseInt(moment.duration(end.diff(start)).as("month")) + 1;
-
-      //  get all month covered
-      let count = 0;
-      let monthsObj = new Array(durationMonth);
-      for (let _year = startYear; _year <= endYear + durationYear; _year++) {
-        let _startMonth = _year === startYear ? startMonth : 0;
-        let _endMonth = _year === endYear ? endMonth : 12;
-        for (let _month = _startMonth; _month <= _endMonth; _month++) {
-          monthsObj[count] = {
-            year: _year,
-            month: _month,
-            amount: 0
-          };
-          count++;
-        }
-      }
-
-      allData.forEach((data) => {
-        const yearMonth = data.month.split("-");
-        const year = parseInt(yearMonth[0]);
-        const month = parseInt(yearMonth[1]);
-        const matchedMonth = monthsObj.filter((_data) => {
-          return _data.year === year && _data.month === month;
-        })[0];
-        matchedMonth && (matchedMonth.amount = data.amount);
-      });
-
-      // case for length 0
-      if (monthsObj.length === 0) success(0);
-      //  case for length 1
-      else if (monthsObj.length === 1) {
-        const days = moment.duration(end.diff(start)).as("days") + 1;
-        const totalDays = start.daysInMonth();
-
-        success((days / totalDays) * monthsObj[0].amount);
-      }
-      // case for multi-monthes
-      else {
-        monthsObj[0].amount *= (start.daysInMonth() - startDay + 1) / start.daysInMonth();
-        monthsObj[monthsObj.length - 1].amount *= endDay / end.daysInMonth();
-
-        success(monthsObj.reduce((acc, _month) => acc + _month.amount, 0));
-      }
+    this.api.budget.all((budget) => {
+      let monthsObj = genEmptyMonths(start, end);
+      dataApplyToMonths(budget, monthsObj);
+      success(calcSum(start, end, monthsObj));
     });
   }
 }
